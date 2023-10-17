1659d71651ee199db7fb9c8f8874da94b46fbcaf
Emir <emirkan.ulker9@gmail.com>
2023-10-18 02:58:30 +0300

    RadyoEfendim

---
+++ RadyoC#Ornek/RadyoC#Ornek/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadyoC_Ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://37.247.98.8/stream/166/";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://turkmedya.radyotvonline.com/turkmedya/alemfm.stream/playlist.m3u8\r\n";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://vofile.ru/turkey/arabesk_damar_fm/icecast.audio\r\n";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://moondigitaledge.radyotvonline.net/radyolanddoksanlar/playlist.m3u8\r\n";
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://moondigitalmaster.radyotvonline.net/efsane4lu/playlist.m3u8\r\n";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://radio-streaming.net/turkey/giresun-102-5-fm-dinle/playlist.m3u8\r\n";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://yayin.liderhost.net:8028/;\r\n";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://ssl120.radyotelekom.com.tr/8678/stream\r\n";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
